00a733bce30f2c7247fb8052534cad4921a3f4bd
recepilhanli <recepilhanli@hotmail.com>
2023-12-23 16:00:02 +0300

    Parallax

--- Atlantis/Assets/Scripts/Parallax.cs
+++ Atlantis/Assets/Scripts/Parallax.cs
@@ -2,23 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
-{
+/// <summary>
+/// this class is used to manage the parallax effect
+/// From: https://gist.github.com/ashblue/3915ab3d02dbaa6d99dc
+/// </summary>
+public class Parallax : MonoBehaviour {
+	Transform cam; // Camera reference (of its transform)
+	Vector3 previousCamPos;
 
-    [SerializeField, Range(0.1f, 2)] float _Speed = 0.1f;
+	public float distanceX = 0f; // Distance of the item (z-index based) 
+	public float distanceY = 0f;
 
-    private Material _Material;
+	public float smoothingX = 1f; // Smoothing factor of parrallax effect
+	public float smoothingY = 1f;
 
-    void Start()
-    {
-        _Material = GetComponent<Renderer>().material;
-    }
+	void Awake () {
+		cam = Camera.main.transform;
+	}
+	
+	void Update () {
+		if (distanceX != 0f) {
+			float parallaxX = (previousCamPos.x - cam.position.x) * distanceX;
+			Vector3 backgroundTargetPosX = new Vector3(transform.position.x + parallaxX, 
+			                                          transform.position.y, 
+			                                          transform.position.z);
+			
+			// Lerp to fade between positions
+			transform.position = Vector3.Lerp(transform.position, backgroundTargetPosX, smoothingX * Time.deltaTime);
+		}
 
+		if (distanceY != 0f) {
+			float parallaxY = (previousCamPos.y - cam.position.y) * distanceY;
+			Vector3 backgroundTargetPosY = new Vector3(transform.position.x, 
+			                                           transform.position.y + parallaxY, 
+			                                           transform.position.z);
+			
+			transform.position = Vector3.Lerp(transform.position, backgroundTargetPosY, smoothingY * Time.deltaTime);
+		}
 
-    float dist = 0;
-    void Update()
-    {
-        dist += Time.deltaTime * _Speed;
-        _Material.SetTextureOffset("_MainTex", Vector2.right * dist);
-    }
+		previousCamPos = cam.position;	
+	}
 }
