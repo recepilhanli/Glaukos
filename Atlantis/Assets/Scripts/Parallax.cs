b78e11268e643450291a24374a2c4b08d271524d
recepilhanli <recepilhanli@hotmail.com>
2024-01-03 20:46:18 +0300

    Fix Parallax Loading System Glitch

--- Atlantis/Assets/Scripts/Parallax.cs
+++ Atlantis/Assets/Scripts/Parallax.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 /// this class is used to manage the parallax effect
 /// From: https://gist.github.com/ashblue/3915ab3d02dbaa6d99dc
 /// </summary>
-public class Parallax : MonoBehaviour {
+public class Parallax : MonoBehaviour
+{
 	Transform cam; // Camera reference (of its transform)
 	Vector3 previousCamPos;
 
@@ -16,30 +17,40 @@ public class Parallax : MonoBehaviour {
 	public float smoothingX = 1f; // Smoothing factor of parrallax effect
 	public float smoothingY = 1f;
 
-	void Awake () {
+	bool started = false;
+	IEnumerator Start()
+	{
+		yield return new WaitForSeconds(0.25f);
 		cam = Camera.main.transform;
+		started = true;
+		yield return null;
 	}
-	
-	void Update () {
-		if (distanceX != 0f) {
+
+	void Update()
+	{
+		if (!started) return;
+
+		if (distanceX != 0f)
+		{
 			float parallaxX = (previousCamPos.x - cam.position.x) * distanceX;
-			Vector3 backgroundTargetPosX = new Vector3(transform.position.x + parallaxX, 
-			                                          transform.position.y, 
-			                                          transform.position.z);
-			
+			Vector3 backgroundTargetPosX = new Vector3(transform.position.x + parallaxX,
+													  transform.position.y,
+													  transform.position.z);
+
 			// Lerp to fade between positions
 			transform.position = Vector3.Lerp(transform.position, backgroundTargetPosX, smoothingX * Time.deltaTime);
 		}
 
-		if (distanceY != 0f) {
+		if (distanceY != 0f)
+		{
 			float parallaxY = (previousCamPos.y - cam.position.y) * distanceY;
-			Vector3 backgroundTargetPosY = new Vector3(transform.position.x, 
-			                                           transform.position.y + parallaxY, 
-			                                           transform.position.z);
-			
+			Vector3 backgroundTargetPosY = new Vector3(transform.position.x,
+													   transform.position.y + parallaxY,
+													   transform.position.z);
+
 			transform.position = Vector3.Lerp(transform.position, backgroundTargetPosY, smoothingY * Time.deltaTime);
 		}
 
-		previousCamPos = cam.position;	
+		previousCamPos = cam.position;
 	}
 }
