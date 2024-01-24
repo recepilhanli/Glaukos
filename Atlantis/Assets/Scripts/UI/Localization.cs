66f84d6449d94fb92798a4e90e879351963f2c3d
Recep İLHANLI <recepilhanli@hotmail.com>
2024-01-25 02:57:30 +0300

    Localization

--- Atlantis/Assets/Scripts/UI/Localization.cs
+++ Atlantis/Assets/Scripts/UI/Localization.cs
@@ -17,14 +17,20 @@ public class Translation
 
     public static Dictionary<string, Translation> Translations {get; private set;} = new Dictionary<string, Translation>()
     {
+        //Main Menu
         {"Menu_Start", new Translation("Start", "Başla") },
         {"Menu_Continue", new Translation("Continue", "Devam Et") },
         {"Menu_Options", new Translation("Options", "Ayarlar") },
         {"Menu_Quit", new Translation("Quit", "Çıkış") },
         {"Menu_TutorialQuestion", new Translation("Would you like to skip the tutorial?", "Eğitimi atlamak ister misiniz?") },
+        
         {"Yes", new Translation("Yes", "Evet") },
         {"No", new Translation("No", "Hayır") },
-        {"Checkpoint", new Translation("Checkpoint Reached","Kayit Noktasi Alindi")}
+        
+        //Level Manager
+        {"Checkpoint", new Translation("Checkpoint Reached","Kayit Noktasi Alindi")},
+
+
     };
 
     public Translation(string Lang_EN, string Lang_TR)
